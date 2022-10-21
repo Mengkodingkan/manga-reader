bdf9b3d59ea7770fd9cec95d44fd727a3996daaf
faisalProject <mf494284@gmail.com>
2022-10-21 15:08:32 +0700

    added contents

--- frontend/src/component/home.js
+++ frontend/src/component/home.js
@@ -15,69 +15,122 @@ const Home = () => {
             </div>
 
             <div className='contents'>
-                <div className="box">
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
+                <div className='main-contents'>
+                    <div className='popular'>
+                        <div className='box'>
+                            <div className='image-container'>
+                                <img src={ComicsImage} alt="" />
+                                <div className='hover'></div>
+                            </div>
+                            <div className='desc-container'>
+                                <h4>I'll Be Taking A Break For Personal Reasons</h4>
+                                <h5>Chapter 20</h5>
+                                <div className='rating'>
+                                    <div className='stars'>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                    </div>
+                                    <div className='evaluation'>
+                                        <h5>10</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
+                        <div className='box'>
+                            <div className='image-container'>
+                                <img src={ComicsImage} alt="" />
+                                <div className='hover'></div>
+                            </div>
+                            <div className='desc-container'>
+                                <h4>I'll Be Taking A Break For Personal Reasons</h4>
+                                <h5>Chapter 20</h5>
+                                <div className='rating'>
+                                    <div className='stars'>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                    </div>
+                                    <div className='evaluation'>
+                                        <h5>10</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
+                        <div className='box'>
+                            <div className='image-container'>
+                                <img src={ComicsImage} alt="" />
+                                <div className='hover'></div>
+                            </div>
+                            <div className='desc-container'>
+                                <h4>I'll Be Taking A Break For Personal Reasons</h4>
+                                <h5>Chapter 20</h5>
+                                <div className='rating'>
+                                    <div className='stars'>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                    </div>
+                                    <div className='evaluation'>
+                                        <h5>10</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
+                        <div className='box'>
+                            <div className='image-container'>
+                                <img src={ComicsImage} alt="" />
+                                <div className='hover'></div>
+                            </div>
+                            <div className='desc-container'>
+                                <h4>I'll Be Taking A Break For Personal Reasons</h4>
+                                <h5>Chapter 20</h5>
+                                <div className='rating'>
+                                    <div className='stars'>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                    </div>
+                                    <div className='evaluation'>
+                                        <h5>10</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
+                        <div className='box'>
+                            <div className='image-container'>
+                                <img src={ComicsImage} alt="" />
+                                <div className='hover'></div>
+                            </div>
+                            <div className='desc-container'>
+                                <h4>I'll Be Taking A Break For Personal Reasons</h4>
+                                <h5>Chapter 20</h5>
+                                <div className='rating'>
+                                    <div className='stars'>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                    </div>
+                                    <div className='evaluation'>
+                                        <h5>10</h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="desc-conatainer"></div>
                     </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
-                        </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
-                        </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
-                        </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
-                        </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
-                    <div className="item">
-                        <div className="image-container">
-                            <img src={ComicsImage} alt="" />
-                        </div>
-                        <div className="desc-conatainer"></div>
-                    </div>
+                    <div className='update-lates'></div>
                 </div>
-                <div className="box"></div>
+                <div className='sidebar'></div>
             </div>
         </>
     )
