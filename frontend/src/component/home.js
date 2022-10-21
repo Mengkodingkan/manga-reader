7d4cf92768ed5daf90fb1abd346e0cc2289febb1
faisalProject <mf494284@gmail.com>
2022-10-21 09:59:59 +0700

    partial update

--- frontend/src/component/home.js
+++ frontend/src/component/home.js
@@ -1,4 +1,5 @@
 import Header from './header';
+import ComicsImage from './assets/img/carousel-1.jpg';
 
 const Home = () => {
     return (
@@ -11,6 +12,72 @@ const Home = () => {
                     <li className='image'></li>
                     <li className='image'></li>
                 </ul>
+            </div>
+
+            <div className='contents'>
+                <div className="box">
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                    <div className="item">
+                        <div className="image-container">
+                            <img src={ComicsImage} alt="" />
+                        </div>
+                        <div className="desc-conatainer"></div>
+                    </div>
+                </div>
+                <div className="box"></div>
             </div>
         </>
     )
