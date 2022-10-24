6f2b03f17cb17c440fa3cfb17648e06f8491137e
faisalProject <mf494284@gmail.com>
2022-10-24 12:35:12 +0700

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './component/header';
 import Home from './component/home';
 import Bookmarks from './component/bookmarks';
 import Comics from './component/comics';
+import Footer from './component/footer';
 import './component/assets/css/styles.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -19,6 +20,8 @@ function App() {
           <Route path='/comics' element={<Comics />}></Route>
         </Routes>
       </BrowserRouter>
+
+      <Footer />
     </>
 
   );
