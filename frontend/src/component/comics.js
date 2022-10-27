a579661e6882d1382cacb5f9b1cfdc3f15684f08
Reiaslite <raidearaujo06@gmail.com>
2022-10-27 15:29:10 +0700

    added comics

--- frontend/src/component/comics.js
+++ frontend/src/component/comics.js
@@ -1,9 +1,58 @@
 import Header from './Header';
+// import Sidebar from './Sidebar';
+import Img from './assets/img/7.webp';
 
 const Comics = () => {
     return (
         <>
             <Header />
+            <div className='content-comics'>
+                <div className='main-content'>
+                    <div className='comics-container'>
+                        <div className='item'>
+                            <div className='img-container'>
+                            <img src={Img} />
+                            </div>
+                            <div className='desc-container'>
+
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='img-container'>
+                                <img src={Img} />
+                            </div>
+                            <div className='desc-container'>
+
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='img-container'>
+                                <img src={Img} />
+                            </div>
+                            <div className='desc-container'>
+
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='img-container'>
+                                <img src={Img} />
+                            </div>
+                            <div className='desc-container'>
+
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='img-container'>
+                                <img src={Img} />
+                            </div>
+                            <div className='desc-container'>
+
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </>
     )
 }
