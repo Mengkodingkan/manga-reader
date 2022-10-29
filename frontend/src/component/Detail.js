6f3399a3e2d71249d8ce45db660871d315fc3096
faisalProject <mf494284@gmail.com>
2022-10-29 11:08:50 +0700

    partial update

--- frontend/src/component/Detail.js
+++ frontend/src/component/Detail.js
@@ -1,4 +1,6 @@
 import Header from './Header';
+import Sidebar from './Sidebar';
+import { Link } from 'react-router-dom';
 
 const Detail = () => {
     return (
@@ -6,7 +8,15 @@ const Detail = () => {
             <Header />
 
             <div className='detail-comic'>
-                <div className='main-contents'></div>
+                <div className='main-contents'>
+                    <div className='box'>
+                        <Link to="/" className='f-1'>Manga Reader </Link> <span>&#62;</span> <Link to="/detail" className='f-1'>I'll Be Taking A Break For Personal Reasons</Link>
+                    </div>
+                    <div className='box'></div>
+                    <div className='box'></div>
+                </div>
+
+                <Sidebar />
             </div>
         </>
     )
